2ffe8cd4318ec767e333ae38faad723071661bbd
shubham-jangid <shubhamjangid70@gmail.com>
2019-06-05 23:33:16 +0530

    testing 2

---
+++ DataStructure/dataStructure/sinlgyLinkedList/SinglyLinkedList.java
@@ -0,0 +1,28 @@
+package DataStructure.dataStructure.sinlgyLinkedList;
+
+public class SinglyLinkedList {
+    Node first;
+
+    public void insertFirst(int data){
+        Node node = new Node();
+        node.data = data;
+        node.next=first;
+        first=node;
+    }
+
+    public void showList(){
+        Node current = first;
+        while(current!=null){
+            System.out.println(current.data);
+            current=current.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        SinglyLinkedList a = new SinglyLinkedList();
+        a.insertFirst(4);
+        a.insertFirst(3);
+        a.insertFirst(2);
+        a.showList();
+    }
+}
